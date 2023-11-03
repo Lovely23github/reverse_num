bdd400c8368926bab681a49f4b6a7bf99d62a6d3
Lovely <lovely02012000@gmail.com>
2023-11-03 15:58:27 +0530

    Initial Commit

---
+++ src/Palind.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class Palind {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num=sc.nextInt();
+        int reveNum=0;
+        int org_num=num;
+        while (num > 0) {
+            int remainder = num % 10;
+            reveNum = reveNum * 10 + remainder;
+            num /= 10;
+        }
+        if (org_num==reveNum){
+            System.out.println("Its a Palindrome Number");
+        }
+        else{
+            System.out.println("Not a Palindrome number");
+        }
+
+    }
+}
